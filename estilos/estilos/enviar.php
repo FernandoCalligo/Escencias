74fde7b6f3d53287a0f4cd57387f507050bc4d5e
Fercalligo <Fernandocalligo@gmail.com>
2024-10-21 18:15:18 -0300

    aa

--- estilos/estilos/enviar.php
+++ estilos/estilos/enviar.php
@@ -1,22 +1,10 @@
 <?php
-  if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $destino = "fernandocalligo@gmail.com";
-    $nombre = $_POST["NOMBRE"];
-    $correo = $_POST["CORREO"];
-    $telefono = $_POST["TELEFONO"];
-    $mensaje = $_POST["MENSAJE"];
-    
-    $contenido = "NOMBRE: " . $nombre . "\nCORREO: " . $correo . "\nTELEFONO: " . $telefono . "\nMENSAJE: " . $mensaje;
-    
-    // Envío del correo
-    if (mail($destino, "CONTACTO", $contenido)) {
-      echo "Correo enviado correctamente.";
-    } else {
-      echo "Error al enviar el correo.";
-    }
-    
-    // Redirección a una página de confirmación o a la página principal
-    header("Location: CELINA_GROUP_SEGUROS.html");
-    exit();
-  }
+  $destino= "fernandocalligo@gmail.com";
+  $nombre = $_POST["NOMBRE"];
+  $correo = $_POST["CORREO"];
+  $telefono = $_POST["TELEFONO"];
+  $mensaje = $_POST["MENSAJE"];
+  $contenido = "NOMBRE". $nombre . "\nCORREO: ". $correo .  "\TELEFONO: " . $telefono . "\MENSAJE: " . $mensaje;
+  mail($destino, "CONTACTO", $contenido);
+  header("location: CELINA GROUP SEGUROS.html");
 ?>
